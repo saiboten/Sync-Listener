3b1d10220b7f4cd233d42a5efd7b6b4cb641ceba
saiboten <saiboten@gmail.com>
2015-04-16 00:09:46 +0200

    Minor visual changes, and if you add a new song, its added to the front of the playlist

--- modules/storage/songrepo.js
+++ modules/storage/songrepo.js
@@ -14,7 +14,7 @@ var add_track_to_playlist = function(playlist_id, song, callback) {
             playlist_repo.get_playlist(playlist_id, function(playlist) {
                 console.log("songrepo:: Found playlist: ", playlist);
                 if(playlist) {
-                    playlist.songs.push(song);
+                    playlist.songs.unshift(song);
                     playlist_repo.save_playlist(playlist, function() {
                         callback(true);
                     });
